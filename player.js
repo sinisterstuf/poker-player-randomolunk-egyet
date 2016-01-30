1fc3f8672b98abc54440e4ab0f37d1d96ddf6219
Akos Maurer-Somogyi <mrakosms@gmail.com>
2016-01-30 11:13:29 +0100

    logic

--- player.js
+++ player.js
@@ -9,10 +9,14 @@ module.exports = {
   bet_request: function(game_state, bet) {
     try {
       ours = game_state.players[game_state.in_action].hole_cards;
-      if (ours[0] < 6 && ours[1] < 6) {
-        bet(10);
-      } else {
+      if (ours[0] === ours[1]) {
         bet(10000);
+      } else if (ours[0] < 6 && ours[1] < 6) {
+        bet(10);
+      } else if (ours[0] < 10 && ours[1] < 10) {
+        bet(200)
+      } else {
+        bet(10000)
       }
     } catch (e) {
       console.log(e);
