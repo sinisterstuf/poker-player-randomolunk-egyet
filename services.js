f3cee9c516db8e62690367841992d6859e99ec02
Balint Bejczi <bejczi.balint@gmail.com>
2016-01-30 16:44:58 +0100

    func

--- services.js
+++ services.js
@@ -61,7 +61,7 @@ function cardholder(game_state) {
     });
     cards.push(checkCard(ours[0].rank));
     cards.push(checkCard(ours[1].rank));
-    return cards.sort(function(a,b) return a-b;);
+    return cards.sort(function(a,b) {return a-b;});
 }
 
 function findPair(cards) {
