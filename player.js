8b82a5e27e15bebdcd90de827640d67d1540c2c7
Akos Maurer-Somogyi <mrakosms@gmail.com>
2016-01-30 10:31:42 +0100

    except

--- player.js
+++ player.js
@@ -4,7 +4,12 @@ module.exports = {
   VERSION: "Randomolunk Egyet",
 
   bet_request: function(game_state, bet) {
-    bet(10);
+    try {
+      bet(100);
+    } catch (e) {
+      console.log(e);
+      bet(0);
+    }
   },
 
   showdown: function(game_state) {
