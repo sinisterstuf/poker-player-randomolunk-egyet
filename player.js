37e39ee75cfcca2f8ad1327ce90e6db8f5a197ce
Balint Bejczi <bejczi.balint@gmail.com>
2016-01-30 11:56:01 +0100

    add calBet

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var checkCard = require("./services.js");
+var services = require("./services.js");
 
 module.exports = {
 
@@ -8,16 +8,7 @@ module.exports = {
 
   bet_request: function(game_state, bet) {
     try {
-      var ours = game_state.players[game_state.in_action].hole_cards;
-      if (ours[0] === ours[1]) {
-        bet(10000);
-      } else if (ours[0] < 6 && ours[1] < 6) {
-        bet(10);
-      } else if (ours[0] < 10 && ours[1] < 10) {
-        bet(200)
-      } else {
-        bet(10000)
-      }
+      bet(services.calBet(game_state))
     } catch (e) {
       console.log(e);
       bet(0);
--- services.js
+++ services.js
@@ -21,6 +21,21 @@ function checkCard(card) {
         }
     }
 
+function calculateBet(game_state) {
+    var ours = game_state.players[game_state.in_action].hole_cards;
+      if (ours[0] === ours[1]) {
+        return 100000;
+      } else if (ours[0] < 6 && ours[1] < 6) {
+        return 10;
+      } else if (ours[0] < 10 && ours[1] < 10) {
+        return 200;
+      } else {
+        return 100000;
+      }
+}
+
+
 module.exports = {
     checkCard: checkCard,
+    calBet: calculateBet,
 }
