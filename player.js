c038cad92ee4a6dfe9c5f41c47dca34798537210
Siôn Le Roux <sinisterstuf@gmail.com>
2016-01-30 10:39:34 +0100

    Avoide low cards or go all in

--- player.js
+++ player.js
@@ -5,7 +5,12 @@ module.exports = {
 
   bet_request: function(game_state, bet) {
     try {
-      bet(100);
+      ours = game_state.players[game_state.in_action].hole_cards;
+      if (ours[0] < 6 && ours[1] < 6) {
+        bet 10;
+      } else {
+        bet 10000;
+      }
     } catch (e) {
       console.log(e);
       bet(0);
