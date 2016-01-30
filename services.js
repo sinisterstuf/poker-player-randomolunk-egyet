d6303e85cb047669430aa3403f45d9b0577531c9
Siôn Le Roux <sinisterstuf@gmail.com>
2016-01-30 15:33:13 +0100

    bring back old logic

--- services.js
+++ services.js
@@ -5,33 +5,52 @@ function minbet(gs) {
 var l = console.log;
 
 function checkCard(card) {
-        switch (card) {
-            case "J" :
-                card = 11;
-                break;
-            case "Q" :
-                card = 12;
-                break;
-            case "K" :
-                card = 13;
-                break;
-            case "A" :
-                card = 14;
-                break;
-            default:
-                card = card;
-        }
-        return card;
-    }
+  switch (card) {
+    case "J" :
+      card = 11;
+      break;
+    case "Q" :
+      card = 12;
+      break;
+    case "K" :
+      card = 13;
+      break;
+    case "A" :
+      card = 14;
+      break;
+    default:
+      card = card;
+  }
+  return card;
+}
 
 function calculateBet(gs) {
-<<<<<<< HEAD
-    console.log(minbetgs);
-    console.log(randomRaise(ranking(gs)));
-      return minbet(gs) + randomRaise(ranking(gs));
-=======
-      return minbet(gs) + raise(ranking(gs));
->>>>>>> 414f8b642fa61962854e320f64e9b000aefc23d5
+  var bet;
+  var r = ranking(gs)
+  if (r) {
+    return minbet(gs) + raise(r);
+  } else {
+    return highCards(gs);
+  }
+}
+
+function highCards(gs) {
+  var ours = gs.players[gs.in_action].hole_cards;
+  var card1 = checkCard(ours[0].rank);
+  var card2 = checkCard(ours[1].rank);
+
+  if (card1 < 2 && card2 < 2) {
+    return 10;
+  } else if (card1 < 6 && card2 < 6 && !lotsOfMoney(gs)) {
+    l("low cards");
+    return minbet(gs);
+  } else if (card1 < 10 && card2 < 10 && !lotsOfMoney(gs)) {
+    l("middle cards");
+    return minbet(gs) + 200;
+  } else {
+    l("high cards");
+    return minbet(gs) + 100000;
+  }
 }
 
 function cardholder(game_state) {
